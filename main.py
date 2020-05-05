5641df2032961fbe32ff65cc018090835debca7c
James Deacon <jamesdeacon314@gmail.com>
2020-05-04 22:58:22 -0700

    HW3

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ from torch.optim.lr_scheduler import StepLR
 from torch.utils.data.sampler import SubsetRandomSampler
 
 import os
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from sklearn.manifold import TSNE
+import pandas as pd
+import seaborn as sn
+import scipy
 
 '''
 This code is adapted from two sources:
@@ -83,9 +91,36 @@ class Net(nn.Module):
     '''
     def __init__(self):
         super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=4, kernel_size=(3,3), stride=1)
+        self.conv2 = nn.Conv2d(4, 16, 3, 1)
+        self.dropout1 = nn.Dropout2d(0.3)
+        self.dropout2 = nn.Dropout2d(0.2)
+        self.fc1 = nn.Linear(400, 180)
+        self.fc2 = nn.Linear(180, 64)
+        self.fc3 = nn.Linear(64, 10)
 
     def forward(self, x):
-        return x
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = self.dropout2(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = self.dropout2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+
+        output = F.log_softmax(x, dim=1)
+        return output
 
 
 def train(args, model, device, train_loader, optimizer, epoch):
@@ -112,6 +147,8 @@ def test(model, device, test_loader):
     test_loss = 0
     correct = 0
     test_num = 0
+    confusion_matrix = np.zeros((10, 10))
+    first_step = True
     with torch.no_grad():   # For the inference step, gradient is not computed
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
@@ -119,13 +156,45 @@ def test(model, device, test_loader):
             test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
+            indices = [i for i, x in enumerate(pred.eq(target.view_as(pred))) if x == False]
+            print(indices)
+            for i in range(len(data)):
+                confusion_matrix[pred[i], target.view_as(pred)[i]] += 1
             test_num += len(data)
+
+            if first_step:
+                total_output = np.array(output)
+                labels = np.array(target.view_as(pred))
+            else:
+                total_output = np.vstack((total_output, output))
+                labels = np.vstack((labels, np.array(target.view_as(pred))))
+            first_step = False
+
+        '''
+        tsne = TSNE(n_components=2, random_state=0)
+        tsne_data = tsne.fit_transform(total_output)
+
+        tsne_data = np.hstack((tsne_data, labels))
+        tsne_df = pd.DataFrame(data=tsne_data, columns=('Dim_1', 'Dim_2', 'label'))
+
+        sn.FacetGrid(tsne_df, hue='label', size=6).map(plt.scatter, 'Dim_1', 'Dim_2').add_legend()
+        plt.show()
+        '''
+
+        feature_nums = [0, 62, 5288]
+        for feature_num in feature_nums:
+            feature = total_output[feature_num].reshape(1, 10)
+            ary = np.array(scipy.spatial.distance.cdist(total_output, feature, metric='euclidean')).reshape(10000)
+            print(ary.argsort()[:9])
+
 
     test_loss /= test_num
 
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, test_num,
         100. * correct / test_num))
+
+    return confusion_matrix
 
 
 def main():
@@ -172,8 +241,14 @@ def main():
         assert os.path.exists(args.load_model)
 
         # Set the test model
-        model = fcNet().to(device)
+        # model = fcNet().to(device)
+        model = Net().to(device)
         model.load_state_dict(torch.load(args.load_model))
+
+        for i in range(model.conv1.weight.shape[0]):
+            img = Image.fromarray(model.conv1.weight[i, 0, :, :].detach().numpy() * 255)
+            img = img.convert("L")
+            img.save("kernel_" + str(i) + ".jpg")
 
         test_dataset = datasets.MNIST('../data', train=False,
                     transform=transforms.Compose([
@@ -182,15 +257,74 @@ def main():
                     ]))
 
         test_loader = torch.utils.data.DataLoader(
-            test_dataset, batch_size=args.test_batch_size, shuffle=True, **kwargs)
+            test_dataset, batch_size=args.test_batch_size, shuffle=False, **kwargs)
 
-        test(model, device, test_loader)
+        img, _ = test_dataset[62]
+        plt.imshow(img.numpy()[0], cmap='gray')
+        plt.show()
+
+        img, _ = test_dataset[1003]
+        plt.imshow(img.numpy()[0], cmap='gray')
+        plt.show()
+
+        img, _ = test_dataset[2018]
+        plt.imshow(img.numpy()[0], cmap='gray')
+        plt.show()
+
+        img, _ = test_dataset[3005]
+        plt.imshow(img.numpy()[0], cmap='gray')
+        plt.show()
+
+        img, _ = test_dataset[4007]
+        plt.imshow(img.numpy()[0], cmap='gray')
+        plt.show()
+
+        img, _ = test_dataset[5288]
+        plt.imshow(img.numpy()[0], cmap='gray')
+        plt.show()
+
+        img, _ = test_dataset[6065]
+        plt.imshow(img.numpy()[0], cmap='gray')
+        plt.show()
+
+        img, _ = test_dataset[7432]
+        plt.imshow(img.numpy()[0], cmap='gray')
+        plt.show()
+
+        img, _ = test_dataset[9905]
+        plt.imshow(img.numpy()[0], cmap='gray')
+        plt.show()
+
+        for i in [0, 8455, 9288, 7082, 494, 2565, 4474, 4811, 6640]:
+            img, _ = test_dataset[i]
+            plt.imshow(img.numpy()[0], cmap='gray')
+            plt.show()
+        for i in [62, 5997, 241, 2573, 6165, 1467, 5719, 2369, 4355]:
+            img, _ = test_dataset[i]
+            plt.imshow(img.numpy()[0], cmap='gray')
+            plt.show()
+        for i in [5288, 1055, 9652, 2024, 5654, 7208, 3755, 7902, 3376]:
+            img, _ = test_dataset[i]
+            plt.imshow(img.numpy()[0], cmap='gray')
+            plt.show()
+
+        confusion_matrix = test(model, device, test_loader)
+
+        # tsne = TSNE(n_components=2, random_state=0)
+        # tsne_data = model.fit_transform(data_1000)
+
+
+        # img = Image.fromarray(confusion_matrix)
+        # print(confusion_matrix)
+        # img = img.convert("L")
+        # img.save("confusion_matrix.jpg")
 
         return
 
     # Pytorch has default MNIST dataloader which loads data at each iteration
     train_dataset = datasets.MNIST('../data', train=True, download=True,
                 transform=transforms.Compose([       # Data preprocessing
+                    transforms.RandomRotation(degrees=10), # Data augmentation
                     transforms.ToTensor(),           # Add data augmentation here
                     transforms.Normalize((0.1307,), (0.3081,))
                 ]))
@@ -199,8 +333,15 @@ def main():
     # training by using SubsetRandomSampler. Right now the train and validation
     # sets are built from the same indices - this is bad! Change it so that
     # the training and validation sets are disjoint and have the correct relative sizes.
-    subset_indices_train = range(len(train_dataset))
-    subset_indices_valid = range(len(train_dataset))
+
+    indicies = np.arange(len(train_dataset))
+    random.Random(148).shuffle(indicies)
+    split_index = int(len(train_dataset) * 0.15)
+    subset_indices_train = indicies[:-split_index]
+    subset_indices_valid = indicies[-split_index:]
+
+    #subset_indices_train = range(len(train_dataset))
+    #subset_indices_valid = range(len(train_dataset))
 
     train_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=args.batch_size,
@@ -212,7 +353,8 @@ def main():
     )
 
     # Load your model [fcNet, ConvNet, Net]
-    model = ConvNet().to(device)
+    # model = ConvNet().to(device)
+    model = Net().to(device)
 
     # Try different optimzers here [Adam, SGD, RMSprop]
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
